de89ffbe33aa08242f275a4800038f88d55f76a2
yp-Leo <you@example.com>
2019-10-10 15:16:35 +0800

    second push

--- tang_dynasty/wsgi.py
+++ tang_dynasty/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Tang_dynasty project.
+WSGI config for tang_dynasty project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tang_dynasty.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tang_dynasty.settings")
 
 application = get_wsgi_application()
